2438a683100df745d9ba491f10947176c94dc123
daha-la <132891041+daha-la@users.noreply.github.com>
2025-01-08 17:31:17 +0100

    even more README

--- Run/gridsearch/RUN_rf_GridSearch.sh
+++ Run/gridsearch/RUN_rf_GridSearch.sh
@@ -4,22 +4,16 @@ blosum62Amb="encodings/blosum62Amb.tsv"
 strucinform="encodings/h26_50sph_blosum.tsv"
 gnn_embedding='encodings/progres_Nt.tsv'
 
-n_estimators="400 800 1000 1200 1600" #600 1400
-#n_estimators=" 400"
+n_estimators="400 800 1000 1200 1600"
 criterion=" gini entropy "
-#criterion=" entropy "
 max_features=" sqrt log2 "
-#max_features=" log2 "
-max_depth="20 50 100" #200
-#max_depth='20'
+max_depth="20 50 100"
 min_samples_split=" 2 5 10 "
-#min_samples_split=" 2 "
 min_samples_leaf=" 1 2 4 "
-#min_samples_leaf=" 1 "
 eval_metric='auc-roc'
 cv_search=5
-outparam=results/hyperparam_tuning/strucinform/best_params/progres_Nt_rf_cv${cv_search}_${eval_metric}
-cv_results=results/hyperparam_tuning/strucinform/cv_results/progres_Nt_rf_cv${cv_search}_${eval_metric}
+outparam=../../Results/hyperparam_tuning/best_params/progres_Nt_rf_cv${cv_search}_${eval_metric}
+cv_results=../../Results/hyperparam_tuning/cv_results/progres_Nt_rf_cv${cv_search}_${eval_metric}
 random_state=42
 
 
---
+++ src/README.md
@@ -0,0 +1,12 @@
+# src
+This folder contains most of the source code used in the different GASP pipelines. Please refer to the individual files for more information about their content.
+
+## Model Architectures
+The folder containing the code for all model architectures utilized by GASP2.0. Also contains the code for doing hyperparameter tuning using gridsearches.
+
+## degnlib  
+This folder includes source code from **degnlib**, a library created by [Christian Madsen Degnbol](https://github.com/degnbol), which was used in the original GASP1.0 pipelines.  
+
+The authors of GASP2.0 claim **no ownership** over the contents of this folder, and it is **not used in any GASP2.0 workflows**. For more information or to obtain the latest version of degnlib, please refer to the [Bitbucket repository](https://bitbucket.org/Degnbol/degnlib).  
+
+If you use the code in this folder, please ensure compliance with any licensing terms provided by the original repository.
